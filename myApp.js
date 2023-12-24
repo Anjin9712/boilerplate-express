fd836a4bfe3107d50dba0f194da3050af5948a1e
Kvothe <hoyosf250@gmail.com>
2023-12-24 13:28:24 -0500

    Practice use & get methods

--- myApp.js
+++ myApp.js
@@ -3,4 +3,15 @@ let app = express();
 
 console.log("Hello World")
 
+app.get("/", (req, res) => {
+    const absolutePath = __dirname + "/views/index.html";
+    res.sendFile(absolutePath);
+})
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) => {
+    res.json({"message": "Hello json"})
+})
+
  module.exports = app;
